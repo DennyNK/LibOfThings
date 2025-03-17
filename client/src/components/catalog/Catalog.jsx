03b69d7861b54656c22d875b8c0efa1cde355b89
DennyNK <dnkovacheva@gmail.com>
2025-03-17 15:12:00 +0200

    add spinner
    if fetching data takes too long

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid, Text,} from '@chakra-ui/react'
+import { Box, SimpleGrid, Spinner} from '@chakra-ui/react'
 import { useEffect, useState } from 'react';
 import thingsService from '../../services/thingsService.js';
 import CatalogThing from './catalog-thing-temp/CatalogThing.jsx';
@@ -6,22 +6,34 @@ import CatalogThing from './catalog-thing-temp/CatalogThing.jsx';
 export default function Catalog() {
 
    const [things, setThings] = useState([]);
+   const [loading, setLoading] = useState(true);
 
     useEffect( () => {
         thingsService.getAllThings()
-        .then(result => setThings(result));
+        .then(result => {
+            setThings(result);
+            setLoading(false);
+        })
+        .catch(err => {
+            console.error('Error fetching things:', err);
+            setLoading(false);
+        })
     }, [])
 
    return (
                 <Box px={6} py={10} bg="gray.100">
-                   <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8}>
 
-                     {things.length > 0
-                     ? things.map(thing => <CatalogThing key={thing._id} {...thing}/>) 
-                     : <Text>No things</Text>}
+                    {loading
+                    ? ( <Box display="flex" justifyContent="center" alignItems="center" height="100px">
+                        <Spinner size="xl" color="teal.500" />
+                      </Box>)
+                    : (<SimpleGrid columns={{ base: 1, md: 3 }} spacing={8}>
 
-                   </SimpleGrid>
-                 </Box>
+                        {things.map(thing => (<CatalogThing key={thing._id} {...thing}/>))}
+   
+                      </SimpleGrid>
+                      )}
 
-   );
+                    </Box>
+            );
 }
