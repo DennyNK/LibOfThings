a1252aa69d8f96a8968446b6b374fbe546822e9c
DennyNK <dnkovacheva@gmail.com>
2025-03-17 12:20:21 +0200

    adapting design of buttons in header and footer desing

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,20 +2,14 @@ import { Box, Flex, Text, Link } from '@chakra-ui/react';
 
 export default function Footer() {
    return (
-    <Box bg="peach.500" color="white" py={4}>
+    <Box bg="peach.400" color="white" py={4}>
     <Flex justify="center" direction="column" align="center">
       <Text fontSize="sm">
-        &copy; 2025 My Website. All rights reserved.
+      The Library of things is a free to use platform. 
       </Text>
       <Flex mt={2} gap={4}>
         <Link href="#" color="white" _hover={{ textDecoration: 'underline' }}>
-          Privacy Policy
-        </Link>
-        <Link href="#" color="white" _hover={{ textDecoration: 'underline' }}>
-          Terms of Service
-        </Link>
-        <Link href="#" color="white" _hover={{ textDecoration: 'underline' }}>
-          Contact Us
+          Created by: Denny NK
         </Link>
       </Flex>
     </Flex>
