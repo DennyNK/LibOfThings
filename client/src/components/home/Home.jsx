5b6275dac8a8d8d97dfe089cbf3879f4a0b7c55b
DennyNK <dnkovacheva@gmail.com>
2025-04-01 10:38:30 +0300

    update Home page
    Add info, change colors

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,69 +1,80 @@
-import { Box, Heading, Text, SimpleGrid, VStack, Link} from "@chakra-ui/react";
-import { Link as RouterLink } from 'react-router';
-import { categories } from "../../data/categories.js";
-import { purposes } from "../../data/purpose.js";
+import { Box, Heading, Text, HStack} from "@chakra-ui/react";
+
+
 
 export default function Home() {
 
       return (
-        <Box px={6} py={10} bg="gray.100">
-          {/* Welcome Message */}
+        <Box px={6} py={10} bg="gray.50">
+  
           <Box mb={8} textAlign="center">
-            <Heading as="h1" size="2xl" color="teal.500">
+            <Heading as="h1" size="2xl" color="purple.500">
               Welcome to the Library of things!
             </Heading>
-            <Text fontSize="lg" color="gray.600" mt={2}>
+            <Text fontSize="lg" color="peach.500" mt={2}>
               Borrow, lend or give away things.
             </Text>
           </Box>
     
-          {/* Latest Items Section */}
+          
           <Box>
-            <Heading as="h2" size="lg" mb={4} textAlign="center" color="teal.500">
+            <Heading as="h2" size="lg" mb={4} textAlign="center" color="purple.700">
               How it works
             </Heading>
           </Box>
 
-          <Heading as="h2" size="lg" mb={4} textAlign="center" color="teal.500">
-        Browse by Category
-      </Heading>
-      <SimpleGrid columns={{ base: 2, md: 5 }} spacing={6} mb={10}>
-        {categories.map((category) => (
-          <Link as={RouterLink} to={category.link} key={category.name} _hover={{ textDecoration: "none" }}>
-            <VStack
-              bg="white"
-              p={4}
-              borderRadius="lg"
-              boxShadow="md"
-              _hover={{ transform: "scale(1.05)", transition: "0.3s" }}
-            >
-              {/* <Image src={category.image} alt={category.name} boxSize="80px" /> */}
-              <Text fontSize="md" fontWeight="bold" color="gray.700">{category.name}</Text>
-            </VStack>
-          </Link>
-        ))}
-      </SimpleGrid>
+          <HStack spacing={8} justify="center" mt={6}>
 
-      {/* Purposes Section */}
-      <Heading as="h2" size="lg" mb={4} textAlign="center" color="teal.500">
-        Browse by Purpose
-      </Heading>
-      <SimpleGrid columns={{ base: 2, md: 2 }} spacing={6}>
-        {purposes.map((purpose) => (
-          <Link as={RouterLink} to={purpose.link} key={purpose.name} _hover={{ textDecoration: "none" }}>
-            <VStack
-              bg="white"
-              p={4}
-              borderRadius="lg"
-              boxShadow="md"
-              _hover={{ transform: "scale(1.05)", transition: "0.3s" }}
-            >
-              {/* <Image src={purpose.image} alt={purpose.name} boxSize="80px" /> */}
-              <Text fontSize="md" fontWeight="bold" color="gray.700">{purpose.name}</Text>
-            </VStack>
-          </Link>
-        ))}
-      </SimpleGrid>
+        <Box
+          p={6}
+          bg="white"
+          borderRadius="md"
+          boxShadow="md"
+          textAlign="center"
+          maxW="sm"
+        >
+          <Heading as="h3" size="md" color="purple.700" mb={2}>
+            Step 1: Browse or add items
+          </Heading>
+          <Text color="peach.700">
+            Check out the Catalog or Search section.
+            Add things.
+          </Text>
+        </Box>
+
+        <Box
+          p={6}
+          bg="white"
+          borderRadius="md"
+          boxShadow="md"
+          textAlign="center"
+          maxW="sm"
+        >
+          <Heading as="h3" size="md" color="purple.700" mb={2}>
+            Step 2: Exchange messages
+          </Heading>
+          <Text color="peach.700">
+            Send a message to borrow or take an item. Answer to messages for your things.
+          </Text>
+        </Box>
+
+        <Box
+          p={6}
+          bg="white"
+          borderRadius="md"
+          boxShadow="md"
+          textAlign="center"
+          maxW="sm"
+        >
+          <Heading as="h3" size="md" color="purple.700" mb={2}>
+            Step 3: Enjoy
+          </Heading>
+          <Text color="peach.700">
+            Take, borrow or lend things. Always be respecftul.
+          </Text>
+        </Box>
+      </HStack>
+          
 
         </Box>
       );
