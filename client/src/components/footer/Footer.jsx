90b874719dad24f289a04dfbbbd80ead57a53e5c
DennyNK <dnkovacheva@gmail.com>
2025-04-01 11:54:33 +0300

    change colors

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,11 +4,11 @@ export default function Footer() {
    return (
     <Box bg="peach.400" color="white" py={4}>
     <Flex justify="center" direction="column" align="center">
-      <Text fontSize="sm">
+      <Text fontSize="sm" color="purple.600">
       The Library of things is a free to use platform. 
       </Text>
       <Flex mt={2} gap={4}>
-        <Link href="#" color="white" _hover={{ textDecoration: 'underline' }}>
+        <Link href="#" color="purple.600" _hover={{ textDecoration: 'underline' }}>
           Created by: Denny NK
         </Link>
       </Flex>
